7a01c53a5a659500586d1b1bdeac5f33c2586cc1
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-08-18 12:04:56 +0530

    frontend setup

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from "path";
+import tailwindcss from '@tailwindcss/vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),tailwindcss()],
+  resolve:{
+    alias:{
+      "@": path.resolve(__dirname,"/src"),
+      "@pages": path.resolve(__dirname,"/src/pages"),
+      "@components": path.resolve(__dirname,"/src/components"),
+      "@store": path.resolve(__dirname,"/src/store"),
+      
+    }
+  },
+  server:{
+    port:4000
+  }
+})
