b57a5c74fe8ce88a2dfa0c32035479f73501bde5
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-08-20 18:20:09 +0530

    some change

--- src/pages/Dashboard/Department.jsx
+++ src/pages/Dashboard/Department.jsx
@@ -1,21 +1,25 @@
 import { useState } from 'react';
 import { Trash2 } from 'lucide-react';
-import { useCreateMutation, useDeleteDepartmentMutation, useGetDepartmentQuery } from '../../services/Department.service';
+import { useCreateMutation, useDeleteDepartmentMutation, useGetDepartmentQuery, useUpdateDapartmentMutation } from '../../services/Department.service';
 
 export default function DepartmentTable() {
     const [inputValue, setInputValue] = useState('');
-
+    const [clickInput,setClickInput] = useState(false)
+    const [ClickId,setClickId] = useState("")
     const [create] = useCreateMutation();
     const { data, isLoading, refetch } = useGetDepartmentQuery();
+    const [upddatDepart,setUpdateDepart] = useState("")
+
 
     const [deleteDepartment, { isLoading: DepDeleteLoad }] = useDeleteDepartmentMutation();
-
+    const [updateDapartment] = useUpdateDapartmentMutation()
 
     const createHandler = async () => {
         try {
             const res = await create({ name: inputValue }).unwrap();
 
             console.log(res);
+            setInputValue("")
             refetch()
         } catch (error) {
             console.log(error);
@@ -33,9 +37,28 @@ export default function DepartmentTable() {
         }
     };
 
+   const updateHandler = async (id,data) => {
+        try {
+            const res = await updateDapartment({ id, data }).unwrap();
+
+            console.log(res);
+            setClickInput(false)
+            setClickId("")
+            setUpdateDepart("")
+            refetch()
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     const handleKeyDown = (e) => {
         if (e.key === 'Enter' && inputValue.trim() !== '') {
             createHandler(inputValue.trim());
+            setInputValue('');
+        }
+
+         if (e.key === 'Enter' && upddatDepart.trim() !== '') {
+            updateHandler(ClickId,{name:upddatDepart.trim()});
             setInputValue('');
         }
     };
@@ -79,7 +102,16 @@ export default function DepartmentTable() {
                 <div>
                     {data.data.map((dept, index) => (
                         <div key={index} className="flex items-center justify-between px-6 py-3 border-b border-gray-100 hover:bg-gray-50">
-                            <span className="text-gray-700">{dept.name}</span>
+                            <div className="px-6 py-4 border-b border-gray-200">
+                               { clickInput && ClickId === dept._id  ? <input
+                                    type="text"
+                                    placeholder="Add department"
+                                    className="w-[200px] border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 text-sm"
+                                    value={upddatDepart ? upddatDepart : dept.name}
+                                    onChange={(e) => setUpdateDepart(e.target.value)}
+                                    onKeyDown={handleKeyDown}
+                                /> :  <span onClick={()=>{setClickId(dept._id);setClickInput(true)}} className="text-gray-700">{dept.name}</span>}
+                            </div>
                             <button disabled={DepDeleteLoad} onClick={() => deleteHandle(dept._id)} className="text-red-500 hover:text-red-700">
                                 <Trash2 size={18} />
                             </button>
