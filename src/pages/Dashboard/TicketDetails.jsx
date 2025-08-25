706820206a36b62ae7f215417c605ec1dbe3a8f2
deepnapsoft-pankaj <itsybizz@gamil.com>
2025-08-25 18:39:07 +0530

    changes

--- src/pages/Dashboard/TicketDetails.jsx
+++ src/pages/Dashboard/TicketDetails.jsx
@@ -539,9 +539,9 @@ export default function TicketDetails() {
           prevTasks.map((task) =>
             typeof task._id === 'string' && typeof selectedTaskForStatus._id === 'string' && task._id === selectedTaskForStatus._id
               ? {
-                ...task,
-                status: existingStatus ? task.status.map((status) => (status._id === existingStatus._id ? { ...status, status: newTaskStatus } : status)) : [...(task.status || []), result.data],
-              }
+                  ...task,
+                  status: existingStatus ? task.status.map((status) => (status._id === existingStatus._id ? { ...status, status: newTaskStatus } : status)) : [...(task.status || []), result.data],
+                }
               : task,
           ),
         );
@@ -852,7 +852,6 @@ export default function TicketDetails() {
 
               {/* Buttons */}
               <div className="flex justify-end gap-3">
-
                 <button type="submit" className="px-4 py-2 rounded-lg bg-blue-500 text-white hover:bg-blue-700">
                   Submit
                 </button>
@@ -964,6 +963,7 @@ export default function TicketDetails() {
               <button
                 onClick={() => {
                   setShowStatusModal(false);
+
                   setNewStatus('Not Started');
                 }}
                 className="text-gray-500 hover:text-gray-700"
