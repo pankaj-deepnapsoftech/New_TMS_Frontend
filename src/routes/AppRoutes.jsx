2b64a789081dc4a0972ef053631511e856dd6468
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-08-27 13:03:42 +0530

    some change

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -7,19 +7,82 @@ import { DashbaordNavLinks } from "@/constant/dashboardNavigation";
 import TicketDetails from "@pages/Dashboard/TicketDetails";
 import { useSelector } from "react-redux";
 
-const AppRoutes = () => {
+// --------------------- ProtectedRoute ---------------------
+const ProtectedRoute = ({ user, children }) => {
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+  return children;
+};
 
-  const user = useSelector((state) => state.Auth.user)
+// --------------------- PublicRoute ------------------------
+const PublicRoute = ({ user, children }) => {
+  if (user) {
+    return <Navigate to="/" replace />;
+  }
+  return children;
+};
+
+// --------------------- AppRoutes --------------------------
+const AppRoutes = () => {
+  const user = useSelector((state) => state.Auth.user);
+
   return (
     <Router>
       <Routes>
-        {!user && <> <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/forgot-password" element={<ForgotPasswordPage />} /> </>}
-        {user ? <Route element={<Adminlayout />}>
-          {DashbaordNavLinks.map((item) => <Route key={item.value} path={item.value} element={item.component} />)}
+        {/* ---------- Public Routes ---------- */}
+        <Route
+          path="/login"
+          element={
+            <PublicRoute user={user}>
+              <LoginPage />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <PublicRoute user={user}>
+              <RegisterPage />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/forgot-password"
+          element={
+            <PublicRoute user={user}>
+              <ForgotPasswordPage />
+            </PublicRoute>
+          }
+        />
+
+        {/* ---------- Protected Routes ---------- */}
+        <Route
+          element={
+            <ProtectedRoute user={user}>
+              <Adminlayout />
+            </ProtectedRoute>
+          }
+        >
+          {DashbaordNavLinks.map((item) => (
+            <Route key={item.value} path={item.value} element={item.component} />
+          ))}
           <Route path="/tickets/:ticketId" element={<TicketDetails />} />
-        </Route> : <Route path="*" element={<Navigate to="/login" replace />} />}
+        </Route>
+
+        {/* ---------- Catch All ---------- */}
+        <Route
+          path="*"
+          element={
+            user ? (
+              <h1 className="text-center mt-10 text-xl font-semibold">
+                404 - Page Not Found
+              </h1>
+            ) : (
+              <Navigate to="/login" replace />
+            )
+          }
+        />
       </Routes>
     </Router>
   );
