e046cc8a5a982b4aa311256f78954180a9911766
deepnapsoft-pankaj <itsybizz@gamil.com>
2025-08-28 18:29:21 +0530

    some changes

--- src/pages/Dashboard/TicketDetails.jsx
+++ src/pages/Dashboard/TicketDetails.jsx
@@ -850,7 +850,7 @@ export default function TicketDetails() {
                   setShowTaskStatusModal(false);
                   setSelectedTaskForStatus(null);
                   setNewTaskStatus('Not Started');
-                }}
+                }}   
                 className="text-gray-500 hover:text-gray-700"
               >
                 ✖
